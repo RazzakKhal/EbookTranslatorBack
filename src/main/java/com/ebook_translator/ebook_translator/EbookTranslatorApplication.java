176b3e2e6de6365c502aae0241a940c492775eda
razzak <khalfallah.razzak@gmail.com>
2026-02-21 19:06:56 +0100

    initialization

---
+++ src/main/java/com/ebook_translator/ebook_translator/EbookTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.ebook_translator.ebook_translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebook_translator/ebook_translator/EbookTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebook_translator.ebook_translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
