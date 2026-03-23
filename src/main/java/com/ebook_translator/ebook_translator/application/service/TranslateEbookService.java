13f122d9cbfeda403956d7992934a907ae89f3d8
razzak <khalfallah.razzak@gmail.com>
2026-03-23 22:37:43 +0100

    ebt-46: Faire Resolver/Mapper qui expose une liste de ReadingOrderItem

--- src/main/java/com/ebook_translator/ebook_translator/application/service/TranslateEbookService.java
+++ src/main/java/com/ebook_translator/ebook_translator/application/service/TranslateEbookService.java
@@ -4,6 +4,7 @@ import com.ebook_translator.ebook_translator.application.dto.TranslateEbookComma
 import com.ebook_translator.ebook_translator.application.port.in.TranslateEbookUseCase;
 import com.ebook_translator.ebook_translator.application.port.out.ExtractEpubPort;
 import com.ebook_translator.ebook_translator.domain.service.EpubFormatValidator;
+import com.ebook_translator.ebook_translator.domain.service.ReaderOrderResolver;
 import com.ebook_translator.ebook_translator.infrastructure.adapter.out.storage.opf.ContentOpfParser;
 
 public class TranslateEbookService implements TranslateEbookUseCase {
@@ -11,13 +12,15 @@ public class TranslateEbookService implements TranslateEbookUseCase {
     private final EpubFormatValidator epubFormatValidator;
     private final ExtractEpubPort extractEpubPort;
     private final ContentOpfParser contentOpfParser;
+    private final ReaderOrderResolver readerOrderResolver;
 
     private final String CONTENT_OPF_PATH = "content.opf";
 
-    public TranslateEbookService(EpubFormatValidator epubFormatValidator, ExtractEpubPort extractEpubPort, ContentOpfParser contentOpfParser) {
+    public TranslateEbookService(EpubFormatValidator epubFormatValidator, ExtractEpubPort extractEpubPort, ContentOpfParser contentOpfParser, ReaderOrderResolver readerOrderResolver) {
         this.epubFormatValidator = epubFormatValidator;
         this.extractEpubPort = extractEpubPort;
         this.contentOpfParser = contentOpfParser;
+        this.readerOrderResolver = readerOrderResolver;
     }
 
     @Override
@@ -25,7 +28,8 @@ public class TranslateEbookService implements TranslateEbookUseCase {
         epubFormatValidator.validate(command.filename(), command.content());
         var epubPath = extractEpubPort.unzipEpub(command.content());
         var parsedContentOpf = contentOpfParser.parse(epubPath.resolve(CONTENT_OPF_PATH));
-        System.out.println(parsedContentOpf);
+        var resolver = readerOrderResolver.resolve(parsedContentOpf.manifest(), parsedContentOpf.spine());
+        System.out.println(resolver);
 
     }
 }
